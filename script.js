5f256ee05114864ac4ef8c1d05d959f50f42e272
Meringue-25 <yuki.li3465@gmail.com>
2026-04-29 11:49:50 -0700

    I think its tiem for my hourly commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
+
+const title = document.getElementById("letter-title");
+const title = document.getElementById("letter-title");
